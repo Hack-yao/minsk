2a5fb4abf4c75c49d109eb907c91fb8304a35b4f
ruomu <ruomuyao89@gmail.com>
2020-12-05 19:29:55 +0800

    Add expression evaluator
    This adds the basics we need, a lexer, a parser, and diagnostics that
    will lay the foundation for our compiler.

---
+++ mc/CodeAnalysis/SyntaxKind.cs
@@ -0,0 +1,20 @@
+namespace Minsk.CodeAnalysis
+{
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhiteSpaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression,
+        ParenthesizedExpression
+    }
+
+}
---
+++ mc/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using Minsk.CodeAnalysis;
+
+namespace Minsk
+{
+    /*
+        1 + 2 * 3
+
+          +
+         /  \
+        1    *
+            / \
+             2  3
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showTree = false;
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    return;
+                }
+
+                if (line == "#showTree")
+                {
+                    showTree = !showTree;
+                    Console.WriteLine(showTree ? "Showing parse trees" : "Not showing parse trees");
+                    continue;
+                }
+                else if (line == "#cls")
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+
+
+                // var parser = new Parser(line);
+                // var syntaxTree = parser.Parse();
+
+                var syntaxTree = SyntaxTree.Parse(line);
+
+                if (showTree)
+                {
+                    var color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    PrettyPrint(syntaxTree.Root);
+                    Console.ForegroundColor = color;
+                }
+
+                if (!syntaxTree.Diagnostics.Any())
+                {
+                    Evaluator evaluator = new Evaluator(syntaxTree.Root);
+                    var result = evaluator.Evaluate();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    var color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    foreach (var diag in syntaxTree.Diagnostics)
+                    {
+                        Console.WriteLine(diag);
+                    }
+                    Console.ForegroundColor = color;
+                }
+
+                // var lexer = new Lexer(line);
+
+                // while (true)
+                // {
+                //     var token = lexer.NextToken();
+                //     if (token.Kind == SyntaxKind.EndOfFileToken)
+                //     {
+                //         break;
+                //     }
+                //     Console.Write($"{token.Kind}: '{token.Text}' ");
+                //     if (token.Value != null)
+                //     {
+                //         Console.Write($"{token.Value}");
+                //     }
+                //     Console.WriteLine();
+                // }
+            }
+        }
+
+        static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
+        {
+
+            // └──
+            // ├──
+            //  │
+            var marker = isLast ? "└─" : "├─";
+
+            Console.Write(indent);
+            Console.Write(marker);
+            Console.Write(node.Kind);
+
+            if (node is SyntaxToken t && t.Value != null)
+            {
+                Console.Write(" ");
+                Console.Write(t.Value);
+            }
+            Console.WriteLine();
+
+            indent += isLast ?  "    " : "│   ";
+            var lastChild = node.GetChildren().LastOrDefault();
+            foreach (var child in node.GetChildren())
+            {
+                PrettyPrint(child, indent, child == lastChild);
+            }
+        }
+    }
+
+}
